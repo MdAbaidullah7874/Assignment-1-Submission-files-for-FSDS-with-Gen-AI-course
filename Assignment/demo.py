5a6a0672aa0c22112f03e156057518b2361ff9ef
MdAbaidullah <mdabaidullah@gmail.com>
2025-11-17 00:13:47 +0530

    Added Assignment 2 files and synchronized local changes

---
+++ Assignment/demo.py
@@ -0,0 +1,26 @@
+print("hello world")
+age = 20
+bimm = "abaidullah"
+print(" i am : " + str(bimm) + " my age is : " + str(age))
+
+
+name = "abaidullah"
+age = 20
+print(" i am : " +str(name) + "        my age is : "  + str(age))
+
+
+score = 100
+score += 5
+print(score)
+score -= 3
+print(score)
+
+x = 10 
+y = 12
+print("is x equal to y ?", x == y)
+print("is x not equal to y ?", x != y)
+print("is x grater than y ?", x>y)
+print("is x less than y ?", x<y)
+print("is x greater than or equal to 10 ?", x >= 10)
+print("is x less than or equal to y ?", x <= y)
+
